333a1cb524ea5ae8186e07577707d0b06fd55c87
humeman <camdenjsf@gmail.com>
2024-01-14 18:22:32 -0600

    Add b2 backup and alerting

--- ci/scripts/ansible-check.sh
+++ ci/scripts/ansible-check.sh
@@ -5,4 +5,13 @@ set -e
 . main/ci/scripts/env-setup.sh $1
 
 echo "-- Checking playbook on environment $1 --"
-ansible-playbook -i "${!SSH_HOST}" -u "${!SSH_USER}" --private-key id_rsa --become-password-file sudo --check --diff pr/ansible/main.yml
+ansible-playbook \
+    -i "${!SSH_HOST}" \
+    -u "${!SSH_USER}" \
+    -e @vault \
+    --vault-password-file vault_k \
+    --private-key id_rsa \
+    --become-password-file sudo \
+    --check \
+    --diff \
+    pr/playbooks/main.yml
--- ci/scripts/env-setup.sh
+++ ci/scripts/env-setup.sh
@@ -7,9 +7,13 @@ export SSH_USER="SSH_USER_$1"
 export SSH_KEY="SSH_KEY_$1"
 export SSH_HOST="SSH_HOST_$1"
 export SSH_SUDO="SSH_SUDO_$1"
+export ANSIBLE_VAULT="ANSIBLE_VAULT_$1"
+export ANSIBLE_VAULT_K_="ANSIBLE_VAULT_K_$1"
 
 echo "-- Writing keys --"
 echo "${!SSH_KEY}" > id_rsa
 echo "${!SSH_SUDO}" > sudo
+echo "${!ANSIBLE_VAULT}" > vault
+echo "${!ANSIBLE_VAULT_K}" > vault_k
 
 echo "-- Ready to go! --"
