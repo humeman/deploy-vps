6ecd65ecdb01f9655a200075d7b0d463532713c1
humeman <camdenjsf@gmail.com>
2024-01-15 19:06:22 -0600

    Maybe all of them?

--- playbooks/roles/backblaze/files/b2-backup.sh
+++ playbooks/roles/backblaze/files/b2-backup.sh
@@ -5,6 +5,7 @@ set -e
 # Remove files older than 45 days
 duplicity \
     remove-older-than 45D --force \
+    --no-encryption \
     b2://{{ b2_account }}:{{ b2_key }}@{{ b2_bucket }}/backups
 
 # Perform the backup, make a full backup if it's been over 20 days
@@ -18,8 +19,10 @@ duplicity \
 # Cleanup failures
 duplicity \
     cleanup --force \
+    --no-encryption \
     b2://{{ b2_account }}:{{ b2_key }}@{{ b2_bucket }}/backups
 
 # Show collection-status
 duplicity collection-status \
+    --no-encryption \
     b2://{{ b2_account }}:{{ b2_key }}@{{ b2_bucket }}/backups
