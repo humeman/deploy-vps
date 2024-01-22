606bd13bf25c8e752411e801213760ebd8518621
humeman <camdenjsf@gmail.com>
2024-01-22 12:52:31 -0600

    Needed that one too

--- ci/scripts/env-setup.sh
+++ ci/scripts/env-setup.sh
@@ -6,16 +6,16 @@ echo "-- Expanding host variables --"
 SECRETS="SECRETS_$1"
 JSON=$(echo "${!SECRETS}" | base64 --decode)
 
-export SSH_USER=$(echo $JSON | jq '.user')
-SSH_HOST=$(echo $JSON | jq '.host')
-echo $JSON | jq '.sudo' > sudo
-echo $JSON | jq '.key' | base64 --decode > id_rsa
+export SSH_USER=$(echo $JSON | jq -r '.user')
+SSH_HOST=$(echo $JSON | jq -r '.host')
+echo $JSON | jq -r '.sudo' > sudo
+echo $JSON | jq -r '.key' | base64 --decode > id_rsa
 chmod 0600 id_rsa
 
-BUCKET=$(echo $JSON | jq '.bucket')
-echo $JSON | jq '.sa' | base64 --decode > service.json
+BUCKET=$(echo $JSON | jq -r '.bucket')
+echo $JSON | jq -r '.sa' | base64 --decode > service.json
 
-echo $JSON | jq '.vault_k' > vault_k
+echo $JSON | jq -r '.vault_k' > vault_k
 
 unset ${SECRETS}
 unset JSON
