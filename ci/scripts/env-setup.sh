2be5317eaffdbc477581601e8634ed7d5182f0d7
humeman <camdenjsf@gmail.com>
2024-01-22 12:59:21 -0600

    Well that's fun! :)

--- ci/scripts/env-setup.sh
+++ ci/scripts/env-setup.sh
@@ -10,6 +10,7 @@ export SSH_USER=$(echo $JSON | jq -r '.user')
 SSH_HOST=$(echo $JSON | jq -r '.host')
 echo $JSON | jq -r '.sudo' > sudo
 echo $JSON | jq -r '.key' | base64 --decode > id_rsa
+echo "" >> id_rsa
 chmod 0600 id_rsa
 
 BUCKET=$(echo $JSON | jq -r '.bucket')
